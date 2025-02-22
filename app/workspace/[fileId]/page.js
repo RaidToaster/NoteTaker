98d817272249b1d161a510b9e72812849d9bc2c7
RaidToaster <154775539+RaidToaster@users.noreply.github.com>
2025-02-22 15:45:04 +0700

    feat: +Added pdf viewer

---
+++ app/workspace/[fileId]/page.js
@@ -0,0 +1,37 @@
+"use client"
+import { useParams } from 'next/navigation'
+import React, { useEffect } from 'react'
+import WorkspaceHeader from '../_components/WorkspaceHeader'
+import PdfViewer from '../_components/PdfViewer'
+import { useQuery } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+
+function Workspace() {
+    const { fileId } = useParams()
+    const fileInfo = useQuery(api.fileStorage.GetFileRecord, { fileId: fileId })
+
+    useEffect(
+        () => {
+            console.log('File Info: ', fileInfo)
+        }, [fileInfo])
+    return (
+        <div>
+            <WorkspaceHeader />
+
+            <div className='grid grid-cols-2 gap-5'>
+                {/* ini editor */}
+                <div>
+
+                </div>
+
+                {/* ini pdf */}
+                <div>
+                    <PdfViewer fileUrl={fileInfo?.fileUrl} />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Workspace
---
+++ app/workspace/_components/PdfViewer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PdfViewer({ fileUrl }) {
+    console.log(fileUrl)
+    return (
+        <div>
+            <iframe src={fileUrl + "#toolbar=0"} width="100%" height="90vh" className='h-[90vh]'></iframe>
+        </div>
+    )
+}
+
+export default PdfViewer
