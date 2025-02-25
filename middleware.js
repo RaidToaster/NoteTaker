743ada6ab0198edcec198c6c64c405dfd42c1d0a
RaidToaster <154775539+RaidToaster@users.noreply.github.com>
2025-02-25 09:32:53 +0700

    fix: fixed middleware

--- middleware.js
+++ middleware.js
@@ -3,14 +3,20 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
     '/sign-in(.*)',
     '/sign-up(.*)',
-    '/'
+    '/',
+    '/api/webhook(.*)',
 ])
 
-export default clerkMiddleware(async (auth, request) => {
-    if (!isPublicRoute(request)) {
-        await auth.protect()
+export default clerkMiddleware(async (auth, req) => {
+    if (!isPublicRoute(req)) {
+        const { userId, sessionId, redirectToSignIn } = await auth();
+        if (!userId || !sessionId) {
+            redirectToSignIn();
+        }
+        auth.protect();
     }
-})
+});
+
 
 export const config = {
     matcher: [
